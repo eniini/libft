75ca025d34a17b4540689bed77c337eeb695faf2
Eero Niini <eniini@student.hive.fi>
2020-07-22 00:54:48 +0300

    fixed while loop calling strlen

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: eniini <eniini@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 17:07:22 by eniini            #+#    #+#             */
-/*   Updated: 2020/07/22 00:37:38 by eniini           ###   ########.fr       */
+/*   Updated: 2020/07/22 00:54:18 by eniini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@
 char	*ft_strnstr(const char *stack, const char *ndl, size_t len)
 {
 	size_t	i;
+	size_t	ndlsize;
 
 	i = 0;
 	if (ndl[0] == '\0')
 		return ((char*)&stack[0]);
-	while (*stack != '\0' && len >= ft_strlen(ndl))
+	ndlsize = ft_strlen(ndl);
+	while (*stack != '\0' && len >= ndlsize)
 	{
 		i = 0;
 		while (*(stack + i) == *(ndl + i))
