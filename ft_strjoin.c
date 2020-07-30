1da0be6fc9ed3a13d35c5c2b38459a5481bdc853
eniini <eniini@student.hive.fi>
2020-07-30 16:11:39 +0300

    cleaned up strjoin, indexing into pointers

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,47 +6,28 @@
 /*   By: eniini <eniini@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 14:29:24 by eniini            #+#    #+#             */
-/*   Updated: 2020/07/18 18:49:26 by eniini           ###   ########.fr       */
+/*   Updated: 2020/07/30 16:10:22 by eniini           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-static char	*ft_dblstrcpy(char const *s1, char const *s2, char *s3)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-	{
-		s3[j] = s1[i];
-		i++;
-		j++;
-	}
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		s3[j] = s2[i];
-		i++;
-		j++;
-	}
-	return (s3);
-}
-
-char		*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*concat;
-	size_t	joinlen;
+	char	*bookmark;
 
 	if (!s1 && !s2)
 		return (NULL);
-	joinlen = ft_strlen(s1) + ft_strlen(s2);
-	if (!(concat = (char*)malloc(sizeof(concat) * (joinlen + 1))))
+	if (!(concat = (char*)malloc(sizeof(char) * (ft_strlen(s1) +
+	ft_strlen(s2) + 1))))
 		return (NULL);
-	concat = ft_dblstrcpy(s1, s2, concat);
-	concat[joinlen] = '\0';
-	return (concat);
+	bookmark = concat;
+	while (*s1 != '\0')
+		*(concat++) = *(s1++);
+	while (*s2 != '\0')
+		*(concat++) = *(s2++);
+	*(concat) = '\0';
+	return (bookmark);
 }
